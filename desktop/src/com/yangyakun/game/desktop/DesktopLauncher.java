ccd197d8e6000f5e3baa94830f926b6914a4ab19
android <361176335@qq.com>
2018-02-06 00:41:10 +0800

    游戏开发 初始化

---
+++ desktop/src/com/yangyakun/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.yangyakun.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.yangyakun.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
